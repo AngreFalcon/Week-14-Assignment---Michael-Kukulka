23f49b8307091d98a17c2455611d6bef5113f2ec
Sorre <angrepenguin@gmail.com>
2024-11-27 12:12:00 -0500

    Populated car.hpp and truck.hpp with class definitions as children of class vehicle

--- Week 14 Assignment - Michael Kukulka/Car.hpp
+++ Week 14 Assignment - Michael Kukulka/Car.hpp
@@ -1,2 +1,16 @@
 #pragma once
 #include "Vehicle.hpp"
+
+class Car : public Vehicle {
+public:
+	Car(void) = default;
+	~Car() = default;
+
+	int getNumDoors(void);
+	void setNumDoors(const int numDoors);
+	void displayInfo(void);
+
+private:
+	int numDoors;
+
+};
--- Week 14 Assignment - Michael Kukulka/Truck.hpp
+++ Week 14 Assignment - Michael Kukulka/Truck.hpp
@@ -1,2 +1,16 @@
 #pragma once
 #include "Vehicle.hpp"
+
+class Truck : public Vehicle {
+public:
+	Truck(void) = default;
+	~Truck() = default;
+
+	int getTowCap(void);
+	void setTowCap(const int towCap);
+	void displayInfo(void);
+
+private:
+	int towCap;
+
+};
