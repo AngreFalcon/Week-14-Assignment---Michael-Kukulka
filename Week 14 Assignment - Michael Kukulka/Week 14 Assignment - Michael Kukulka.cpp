adf37892ddd982d2c55a993dd72e1fc69943d4c4
Sorre <angrepenguin@gmail.com>
2024-11-27 12:09:12 -0500

    Add project files.

---
+++ Week 14 Assignment - Michael Kukulka/Week 14 Assignment - Michael Kukulka.cpp
@@ -0,0 +1,12 @@
+// Michael Kukulka
+// CIS 1202 N01
+// November 23, 2024
+
+#include "commonutils.hpp"
+#include "Vehicle.hpp"
+#include "Car.hpp"
+#include "Truck.hpp"
+
+int main() {
+	return EXIT_SUCCESS;
+}
