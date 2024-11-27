6349acfb2e9698a69f2b9f330a175b3e4fd29576
Sorre <angrepenguin@gmail.com>
2024-11-27 12:10:37 -0500

    Populate vehicle.hpp with class members

--- Week 14 Assignment - Michael Kukulka/Vehicle.hpp
+++ Week 14 Assignment - Michael Kukulka/Vehicle.hpp
@@ -1,1 +1,19 @@
 #pragma once
+#include <string.h>
+
+class Vehicle {
+public:
+	Vehicle(void) = default;
+	~Vehicle() = default;
+
+	std::string getManufacturer(void);
+	void setManufacturer(const std::string& manufacturer);
+	int getYear(void);
+	void setYear(const int year);
+	void displayInfo(void);
+
+private:
+	std::string manufacturer;
+	int year;
+
+};
